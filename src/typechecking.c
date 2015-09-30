cae3529d1e788c6b23d351feb9cd4fe2809a47e4
pierreganty <pierreganty@gmail.com>
2015-09-30 10:39:29 +0200

    Fix indentation

--- src/typechecking.c
+++ src/typechecking.c
@@ -32,118 +32,116 @@
 struct guard_list *list = NULL;
 struct guard_list *list_last;
 
-boolean possible_petri_net_check_action(T_PTR_tree entry){
-   //printf("Checking if the node: %s belongs to a petri net \n", (char*)entry->info);
-   if (strcmp((char*)entry->info, "=") == 0){
-      if (entry->nbrsubtrees != 2) return false;
+boolean possible_petri_net_check_action(T_PTR_tree entry) {
+	//printf("Checking if the node: %s belongs to a petri net \n", (char*)entry->info);
+	if (strcmp((char*)entry->info, "=") == 0){
+		if (entry->nbrsubtrees != 2) return false;
 
-      T_PTR_tree child, subchild_var;
-      child = entry->subtrees[1];
+		T_PTR_tree child, subchild_var;
+		child = entry->subtrees[1];
 
-      if (child->nbrsubtrees != 2) return false;
-      subchild_var = child->subtrees[0];
-      int value = atoi(child->subtrees[1]->info);
+		if (child->nbrsubtrees != 2) return false;
+		subchild_var = child->subtrees[0];
+		int value = atoi(child->subtrees[1]->info);
 
-      if (subchild_var->nbrsubtrees != 0) return false;
+		if (subchild_var->nbrsubtrees != 0) return false;
 
-      if (strcmp((char*)entry->subtrees[0]->info,(char*)subchild_var->info) != 0) return false;
+		if (strcmp((char*)entry->subtrees[0]->info,(char*)subchild_var->info) != 0) return false;
 
-      struct guard_list *aux = list;
-      struct guard_list *iter = list;
-      while (iter != NULL){
-         if (strcmp(iter->variable, (char*)subchild_var->info) == 0) aux = iter;
-         iter = iter->next;
-      }
+		struct guard_list *aux = list;
+		struct guard_list *iter = list;
+		while (iter != NULL){
+			if (strcmp(iter->variable, (char*)subchild_var->info) == 0) aux = iter;
+			iter = iter->next;
+		}
 
-      //printf("From the list read %d the rule has %d\n",aux->min_value, value);
-      if (strcmp((char*)child->info, "-") == 0) {
-         if (aux->min_value < value) return false;
-      } else if (strcmp((char*)child->info, "+") == 0){
-         if (aux->max_value < value) return false;
-      }
+		//printf("From the list read %d the rule has %d\n",aux->min_value, value);
+		if (strcmp((char*)child->info, "-") == 0) {
+			if (aux->min_value < value) return false;
+		} else if (strcmp((char*)child->info, "+") == 0){
+			if (aux->max_value < value) return false;
+		}
 
-      return true;
+		return true;
 
-   }
-   return true; // Nothing to do with this node but it doesn't represent any problem
+	}
+	return true; // Nothing to do with this node but it doesn't represent any problem
 }
 
-boolean possible_petri_net_check_guards(T_PTR_tree entry){
-if ((strcmp((char*)entry->info, ">=") == 0) || strcmp((char*)entry->info, "=") == 0){
+boolean possible_petri_net_check_guards(T_PTR_tree entry) {
+	if ((strcmp((char*)entry->info, ">=") == 0) || strcmp((char*)entry->info, "=") == 0){
 
-      T_PTR_tree value_child, name_child;
-      name_child = entry->subtrees[0];
-      value_child = entry->subtrees[1];
+		T_PTR_tree value_child, name_child;
+		name_child = entry->subtrees[0];
+		value_child = entry->subtrees[1];
 
-      // Check if the "=" belong to a guard declaration or to an action one
-      if (value_child->nbrsubtrees != 0) return true;
+		// Check if the "=" belong to a guard declaration or to an action one
+		if (value_child->nbrsubtrees != 0) return true;
 
-      // Create and initializa a new element for the list
-      struct guard_list *new;
-      new = (struct guard_list *)xmalloc(sizeof(struct guard_list));
+		// Create and initializa a new element for the list
+		struct guard_list *new;
+		new = (struct guard_list *)xmalloc(sizeof(struct guard_list));
 
-      new->variable = (char *)xmalloc(strlen(((T_PTR_tbsymbol_entry) name_child->info)->name)+1);
-      strcpy(new->variable,((T_PTR_tbsymbol_entry) name_child->info)->name);
-      new->min_value = atoi((char*)value_child->info);
-      new->max_value = MAX_INT;
-      new->next = NULL;
+		new->variable = (char *)xmalloc(strlen(((T_PTR_tbsymbol_entry) name_child->info)->name)+1);
+		strcpy(new->variable,((T_PTR_tbsymbol_entry) name_child->info)->name);
+		new->min_value = atoi((char*)value_child->info);
+		new->max_value = MAX_INT;
+		new->next = NULL;
 
-     // printf("Adding to the list variable: %s with values:[%d,%d]\n", new->variable, new->min_value,new->max_value);
+		// printf("Adding to the list variable: %s with values:[%d,%d]\n", new->variable, new->min_value,new->max_value);
 
-      // If it's the first call to the function we must initialize the list
-      if (list == NULL) {
-         list = new;
-         list_last = new;
-      } else {
-         list_last->next = new;
-         list_last = new;
-      }
+		// If it's the first call to the function we must initialize the list
+		if (list == NULL) {
+			list = new;
+			list_last = new;
+		} else {
+			list_last->next = new;
+			list_last = new;
+		}
 
 
-   } else if (strcmp((char*)entry->info, "in") == 0){
-      T_PTR_tree value_min_child, value_max_child, name_child;
-      name_child = entry->subtrees[0];
-      value_min_child = entry->subtrees[1];
-      value_max_child = entry->subtrees[2];
+	} else if (strcmp((char*)entry->info, "in") == 0){
+		T_PTR_tree value_min_child, value_max_child, name_child;
+		name_child = entry->subtrees[0];
+		value_min_child = entry->subtrees[1];
+		value_max_child = entry->subtrees[2];
 
-      // Create and initializa a new element for the list
-      struct guard_list *new;
-      new = (struct guard_list *)xmalloc(sizeof(struct guard_list));
+		// Create and initializa a new element for the list
+		struct guard_list *new;
+		new = (struct guard_list *)xmalloc(sizeof(struct guard_list));
 
-      new->variable = (char *)xmalloc(strlen(((T_PTR_tbsymbol_entry) name_child->info)->name)+1);
-      strcpy(new->variable,((T_PTR_tbsymbol_entry) name_child->info)->name);
-      new->min_value = atoi((char*)value_min_child->info);
-      new->max_value = atoi((char*)value_max_child->info);
-      new->next = NULL;
+		new->variable = (char *)xmalloc(strlen(((T_PTR_tbsymbol_entry) name_child->info)->name)+1);
+		strcpy(new->variable,((T_PTR_tbsymbol_entry) name_child->info)->name);
+		new->min_value = atoi((char*)value_min_child->info);
+		new->max_value = atoi((char*)value_max_child->info);
+		new->next = NULL;
 
-      //printf("Adding to the list variable: %s with values:[%d,%d]\n", new->variable, new->min_value,new->max_value);
+		//printf("Adding to the list variable: %s with values:[%d,%d]\n", new->variable, new->min_value,new->max_value);
 
-      // If it's the first call to the function we must initialize the list
-      if (list == NULL) {
-         list = new;
-         list_last = new;
-      } else {
-         list_last->next = new;
-         list_last = new;
-      }
-   }
+		// If it's the first call to the function we must initialize the list
+		if (list == NULL) {
+			list = new;
+			list_last = new;
+		} else {
+			list_last->next = new;
+			list_last = new;
+		}
+	}
 
-   return true; // Nothing to do with this node but it doesn't represent any problem
+	return true; // Nothing to do with this node but it doesn't represent any problem
 }
 
-boolean is_petri_net(T_PTR_tree tree){
-   // To check if we are working with a Petri Net we just have to look at the rules subtree
-   boolean ret = tree_dump(tree->subtrees[0], possible_petri_net_check_guards, possible_petri_net_check_action, NULL);
+boolean is_petri_net(T_PTR_tree tree) {
+	// To check if we are working with a Petri Net we just have to look at the rules subtree
+	boolean ret = tree_dump(tree->subtrees[0], possible_petri_net_check_guards, possible_petri_net_check_action, NULL);
 
-   struct guard_list *iter = list;
-   while (iter != NULL){
-      iter = list->next;
-      xfree(list->variable);
-      xfree(list);
-      list = iter;
-   }
+	struct guard_list *iter = list;
+	while (iter != NULL){
+		iter = list->next;
+		xfree(list->variable);
+		xfree(list);
+		list = iter;
+	}
 
-   return ret;
+	return ret;
 }
-
-
